c7aca985b1308a284e3833bc90cd2234c46a5e00
sap2411 <steven.parsons.us@gmail.com>
2020-04-24 12:28:04 -0700

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,50 @@
 class Startup
+    attr_reader :name, :founder, :domain
+    @@all = []
 
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.find_by_founder(name)
+        all.find {|startup| startup.founder == name}
+    end
+
+    def self.domains
+        all.map {|startup| startup.domain}
+    end
+
+    def sign_contract(vc, type, amount)
+        FundingRound.new(self, vc, type, amount)
+    end
+
+    def num_funding_rounds
+        funding_rounds.count
+    end
+
+    def total_funds
+        funding_rounds.reduce {|sum, round| sum += round.investment ; sum}
+    end
+
+    def investors
+        funding_rounds.map {|round| round.investor}.uniq
+    end
+
+    def big_investors
+        ballers = VentureCapitalist.tres_commas_club #local variable to not repeat class variable calls
+        investors.select {|investor| ballers.include?(investor)}
+    end
+
+    def funding_rounds 
+        FundingRound.select {|round| round.startup == self}
+    end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,37 @@
 class VentureCapitalist
+    attr_reader :name, :total_worth
+    @@all = []
 
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        all.select {|vc| vc.total_worth > 1,000,000,000}
+    end
+
+    def offer_contract(start_up, type, amount)
+        FundingRound.new(start_up, self, type, amount)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|round| round.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map {|round| round.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.sort_by {|round| round.investment}.last
+    end
+
+    def invested(domain)
+        our_startups = portfolio.select {|startup| startup.domain == domain}
+        our_startups.inject {|total, startup| total += startup.total_funds}
+    end
 end
